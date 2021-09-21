507a37ccbb455563fe1dcd40c80a44ea12f4f52a
Rico Zhu <ricozhuthegreat@hotmail.com>
2021-09-21 13:22:15 -0400

    Created Normal Distribution

--- generator.py
+++ generator.py
@@ -1,7 +1,14 @@
 
-import numpy as np
-import os
-import matplotlib as plt
-
+import math
 import torch
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Normal distribution from 0 - 100m
+n_rand = np.random.normal(50.0, 10.0, 100000)
+
+fig, axs = plt.subplots(1, 1)
+axs.hist(n_rand)
+
+plt.show()
