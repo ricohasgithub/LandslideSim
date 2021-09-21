475a2f788c6b49ef18a6c1ea5749f3e2c7d3f17c
Rico Zhu <ricozhuthegreat@hotmail.com>
2021-09-21 13:41:33 -0400

    Added Output Calculations

--- generator.py
+++ generator.py
@@ -5,22 +5,35 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
+in_dim = 1000
+
 # Generate random hw: normal distribution from 0 - 100m
-n_hw = np.random.normal(50.0, 10.0, 100000)
+n_hw = np.random.normal(50.0, 10.0, in_dim)
 # Generate random v0
-n_v0 = np.random.normal(0.5, 0.1, 100000)
+n_v0 = np.random.normal(0.5, 0.1, in_dim)
 # Generate random alpha
-n_alpha = np.random.normal(5.0, math.sqrt(2.0), 100000)
+n_alpha = np.random.normal(5.0, math.sqrt(2.0), in_dim)
+
+# Calculate output values
+l_left_hand_side = []
+for i in range(in_dim):
+    l_left_hand_side.append(n_v0[i] + n_alpha[i] * n_hw[i])
+
+n_left_hand_side = np.array(l_left_hand_side)
 
 # Convert to torch tensors for processing
 hw = torch.tensor(n_hw)
 v0 = torch.tensor(n_v0)
-n_alpha = torch.tensor(n_alpha)
+alpha = torch.tensor(n_alpha)
 
 # Visualize data distribution
-fig, axs = plt.subplots(1, 3, sharey=True)
+fig, axs = plt.subplots(1, 3)
 axs[0].hist(n_hw)
 axs[1].hist(n_v0)
-axs[1].hist(n_alpha)
+axs[2].hist(n_alpha)
+
+# Create input data tensor
+right_hand_side = torch.stack([v0, alpha, hw])
+left_hand_side = torch.tensor(n_left_hand_side)
 
 plt.show()
